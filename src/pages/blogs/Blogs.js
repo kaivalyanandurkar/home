3d5f4f80d70a0d2be5527f7b9d72d5241947b99b
ishitakeshawani <keshwaniishita054@gmail.com>
2021-10-13 13:53:29 +0530

    created react app

---
+++ src/pages/blogs/Blogs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./blogs.css"
+
+export default function Blogs() {
+    return (
+        <div className="Blogs">
+            this is blog page.
+        </div>
+    )
+}
